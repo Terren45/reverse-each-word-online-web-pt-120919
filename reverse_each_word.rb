2538cfb14eea4108c319d29033c4139cfb43baa4
Terren Culberson <culbersonterren@yahoo.com>
2019-12-20 04:39:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   array = sentence.split(" ") 
   test_array = []
-  array.collect do||
-    test_array << string.reverse
+  array.collect do|sentence|
+    test_array << .reverse
   end
   test_array.join(" ")
 end
